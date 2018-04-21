1de6bb97bfaf1a332aaa8e0dcfae1f9158eccd58
Germano Barcelos <germanobarcelos@yahoo.com.br>
2018-04-21 10:16:34 -0300

    Theming bootstrap

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,15 +3,18 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var plumber     = require('gulp-plumber');
+var concat      = require('gulp-concat');
+var minify      = require('gulp-minify-css');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'compress'], function() {
 
     browserSync.init({
         server: "./"
     });
 
     gulp.watch("assets/scss/*.scss", ['sass']);
+    gulp.watch("assets/css/*.css", ['compress'])
     gulp.watch("./*.html").on('change', browserSync.reload);
     gulp.watch("html/*.html").on('change', browserSync.reload);
 });
@@ -24,5 +27,13 @@ gulp.task('sass', function() {
         .pipe(gulp.dest("assets/css"))
         .pipe(browserSync.stream());
 });
+
+gulp.task('compress', function(){
+    return gulp.src('assets/css/*.css')
+        .pipe(concat('style.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('assets/build/css'))
+        .pipe(browserSync.stream());
+ });
 
 gulp.task('default', ['serve']);
