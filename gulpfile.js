c9c074bce782b315f11989abdda5e8b0570e418b
Dener Vieira <deneribeiro10@hotmail.com>
2018-04-25 22:55:32 -0300

    index mobile

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// Main
+gulp.task('default', ['watch']);
+
+// Compile scss
+gulp.task('styles', function() {
+    return gulp.src('assets/sass/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('assets/css/'))
+        .pipe(browserSync.stream());
+});
+
+//Watching files for changes
+gulp.task('watch', function() {
+    browserSync.init({
+        server: './'
+    });
+    gulp.watch('assets/sass/*.scss').on('change', browserSync.reload);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
