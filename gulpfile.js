234cc51ac44aa252202fed60a8c23773561c434b
Dener Vieira <deneribeiro10@hotmail.com>
2018-04-28 01:39:20 -0300

    debugging

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,8 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 // Main
 gulp.task('default', ['watch']);
-
-// Compile scss
-gulp.task('styles', function() {
-    return gulp.src('assets/sass/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('assets/css/'))
-        .pipe(browserSync.stream());
-});
 
 //Watching files for changes
 gulp.task('watch', function() {
